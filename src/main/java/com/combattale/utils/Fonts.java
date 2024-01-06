cf9806ffe988e58b3bceb31d10d7a3304b8ebc93
4whoami4 <kristofer127@interia.pl>
2024-01-06 13:11:39 +0100

    Create Fonts class

---
+++ src/main/java/com/combattale/utils/Fonts.java
@@ -0,0 +1,25 @@
+package com.combattale.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.*;
+
+public class Fonts {
+    public static final BitmapFont TITLE_FONT;
+
+    static {
+        TITLE_FONT = createFont("DeterminationSansWebRegular-369X", new FreeTypeFontParameter() {{
+            size = 72;
+        }});
+    }
+
+    private static BitmapFont createFont(String fontName, FreeTypeFontParameter params) {
+        final FreeTypeFontGenerator generator = new FreeTypeFontGenerator(
+                Gdx.files.internal("fonts/" + fontName + ".ttf")
+        );
+        final BitmapFont font = generator.generateFont(params);
+        generator.dispose();
+        return font;
+    }
+}
