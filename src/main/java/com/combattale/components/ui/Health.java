a10af4c8ee1d3611463c5c8257c6e977ac466f3e
4whoami4 <kristofer127@interia.pl>
2024-01-17 22:57:14 +0100

    Create game over screen

--- src/main/java/com/combattale/components/ui/Health.java
+++ src/main/java/com/combattale/components/ui/Health.java
@@ -1,5 +1,7 @@
 package com.combattale.components.ui;
 
+import com.combattale.Game;
+import com.combattale.scenes.GameOverScene;
 import com.combattale.utils.Fonts;
 
 public class Health extends Text {
@@ -11,11 +13,16 @@ public class Health extends Text {
 
     public void decrease(int amount) {
         health -= amount;
+        if (health <= 0) {
+            health = 0;
+            Game.instance.setScene(new GameOverScene());
+        }
         layout.setText(Fonts.BUTTON_FONT, String.valueOf(health));
     }
 
     public void increase(int amount) {
         health += amount;
+        if (health > 100) health = 100;
         layout.setText(Fonts.BUTTON_FONT, String.valueOf(health));
     }
 }
---
+++ src/main/java/com/combattale/scenes/GameOverScene.java
@@ -0,0 +1,48 @@
+package com.combattale.scenes;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
+import com.combattale.Game;
+import com.combattale.components.ui.Button;
+import com.combattale.components.ui.Text;
+import com.combattale.utils.Component;
+import com.combattale.utils.Fonts;
+import com.combattale.utils.Scene;
+
+import java.util.ArrayList;
+
+public class GameOverScene extends Scene {
+    private final ArrayList<String> buttonTexts = new ArrayList<>() {{
+        add("Retry");
+        add("Exit");
+    }};
+    private final ArrayList<Runnable> buttonActions = new ArrayList<>() {{
+        add(() -> Game.instance.setScene(new FirstStageScene()));
+        add(() -> Game.instance.setScene(new MenuScene()));
+    }};
+
+    private Button createButton(String text, Vector2 offset, Runnable onClickAction) {
+        return new Button(text, Fonts.BUTTON_FONT)
+                .withOnClick(onClickAction)
+                .withPadding(new Vector2(10, 10))
+                .withTextColor(Color.WHITE)
+                .withTextHoverColor(Color.GREEN)
+                .withColor(Color.BLACK)
+                .withHoverColor(Color.BLACK)
+                .withOffset(offset);
+    }
+
+    @Override
+    public ArrayList<Component> getComponents() {
+        final ArrayList<Component> components = new ArrayList<>();
+        components.add(
+                new Text("GameOver", Fonts.TITLE_FONT).withOffset(new Vector2(0, 20))
+        );
+        for (int i = 0; i < buttonTexts.size(); i++) {
+            components.add(
+                    createButton(buttonTexts.get(i), new Vector2(i * 100 - 50, -100), buttonActions.get(i))
+            );
+        }
+        return components;
+    }
+}
