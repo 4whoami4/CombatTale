55dbf01ada7d73538ba5c7fea2d330fc99b11704
4whoami4 <kristofer127@interia.pl>
2024-01-19 11:16:40 +0100

    Update health UI

--- src/main/java/com/combattale/components/ui/Health.java
+++ src/main/java/com/combattale/components/ui/Health.java
@@ -1,14 +1,41 @@
 package com.combattale.components.ui;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.combattale.Game;
 import com.combattale.scenes.GameOverScene;
 import com.combattale.utils.Fonts;
+import com.combattale.utils.GuiComponent;
+import com.combattale.utils.GuiPosition;
 
-public class Health extends Text {
+public class Health extends GuiComponent {
+    private final GlyphLayout layout = new GlyphLayout();
+    private final BitmapFont font = Fonts.BUTTON_FONT;
     private int health = 100;
 
-    public Health() {
-        super("100", Fonts.BUTTON_FONT);
+    @Override
+    public void create() {
+        layout.setText(font, String.valueOf(health));
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch, ShapeRenderer shapeRenderer) {
+        Vector2 pos = calcPosition(GuiPosition.LEFT_BOTTOM, 160, layout.height);
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(0.5f, 0.5f, 0.5f, 1);
+        shapeRenderer.rect(pos.x + 64, pos.y + 14, 128, 21);
+        shapeRenderer.setColor(1, 0, 0, 1);
+        shapeRenderer.rect(pos.x + 64, pos.y + 14, 128 * health / 100f, 21);
+        shapeRenderer.end();
+
+        spriteBatch.begin();
+        font.draw(spriteBatch, "HP", pos.x + 32, pos.y + 32);
+        font.draw(spriteBatch, layout, pos.x + 68, pos.y + 32);
+        spriteBatch.end();
     }
 
     public void decrease(int amount) {
@@ -17,12 +44,12 @@ public class Health extends Text {
             health = 0;
             Game.instance.setScene(new GameOverScene());
         }
-        layout.setText(Fonts.BUTTON_FONT, String.valueOf(health));
+        layout.setText(font, String.valueOf(health));
     }
 
     public void increase(int amount) {
         health += amount;
         if (health > 100) health = 100;
-        layout.setText(Fonts.BUTTON_FONT, String.valueOf(health));
+        layout.setText(font, String.valueOf(health));
     }
 }
