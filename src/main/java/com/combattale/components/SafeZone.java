dc0e946567f66232ca44e06eb4bd4c0a750ad7bd
4whoami4 <kristofer127@interia.pl>
2024-01-26 13:47:36 +0100

    Add safezone pattern

--- src/main/java/com/combattale/components/SafeZone.java
+++ src/main/java/com/combattale/components/SafeZone.java
@@ -17,24 +17,60 @@ public class SafeZone extends Component {
     private static final boolean SHOW = true;
 
     private static final TimedList<Rectangle> sizes = new TimedList<>() {{
-        add(0f, new Rectangle(0, 0, WIDTH, HEIGHT));
-        add(4f, new Rectangle(0, 0, WIDTH, 70));
-        add(7f, new Rectangle(0, HEIGHT - 70, WIDTH, 70));
-        add(10f, new Rectangle(0, 0, WIDTH, 70));
-        add(13f, new Rectangle(0, HEIGHT - 70, WIDTH, 70));
+        add(0f, new Rectangle(0, 0, WIDTH, HEIGHT)); //full green
+        add(1.3f, new Rectangle(0, 0, WIDTH, 70)); //green up
+        add(2.6f, new Rectangle(0, HEIGHT - 70, WIDTH, 70)); //green down
+        add(3.9f, new Rectangle(0, 0, WIDTH - 200, HEIGHT)); // green left
+        add(5.2f, new Rectangle(WIDTH - 200, 0, 200, HEIGHT)); // green right
+
+
+        add(7.2f, new Rectangle(0, 0, WIDTH, HEIGHT)); //full green
+        add(8.5f, new Rectangle(0, 0, WIDTH - 200, HEIGHT)); // green left
+        add(9.8f, new Rectangle(0, HEIGHT - 70, WIDTH, 70)); //green down
+        add(11.1f, new Rectangle(WIDTH - 200, 0, 200, HEIGHT)); // green right
+        add(12.4f, new Rectangle(0, 0, WIDTH, 70)); //green up
+
+
+        add(14.4f, new Rectangle(0, 0, WIDTH, HEIGHT)); //full green
+        add(15.7f, new Rectangle(0, 0, WIDTH, 70)); //green up
+        add(18f, new Rectangle(0, HEIGHT - 70, WIDTH, 70)); //green down
+        add(19.3f, new Rectangle(0, 0, WIDTH - 200, HEIGHT)); // green left
+        add(20.6f, new Rectangle(WIDTH - 200, 0, 200, HEIGHT)); // green right
+
+        add(22.6f, new Rectangle(0, 0, WIDTH, HEIGHT)); //full green
+        add(23.9f, new Rectangle(0, 0, WIDTH, 70)); //green up
+        add(25.2f, new Rectangle(0, HEIGHT - 70, WIDTH, 70)); //green down
+        add(26.5f, new Rectangle(0, 0, WIDTH - 200, HEIGHT)); // green left
+        add(27.8f, new Rectangle(WIDTH - 200, 0, 200, HEIGHT)); // green right
+
+        add(29.8f, new Rectangle(0, 0, WIDTH, HEIGHT)); //full green
+        add(30.9f, new Rectangle(0, 0, WIDTH, 70)); //green up
+        add(32.2f, new Rectangle(0, HEIGHT - 70, WIDTH, 70)); //green down
+        add(33.5f, new Rectangle(0, 0, WIDTH - 200, HEIGHT)); // green left
+        add(34.8f, new Rectangle(WIDTH - 200, 0, 200, HEIGHT)); // green right
+
+        add(36.8f, new Rectangle(0, 0, WIDTH, HEIGHT)); //full green
+        add(38.1f, new Rectangle(0, 0, WIDTH, 70)); //green up
+        add(41.4f, new Rectangle(0, HEIGHT - 70, WIDTH, 70)); //green down
+        add(42.7f, new Rectangle(0, 0, WIDTH - 200, HEIGHT)); // green left
+        add(46f, new Rectangle(WIDTH - 200, 0, 200, HEIGHT)); // green right
+
+        add(49f, new Rectangle(0 , 0, 0, 0)); // dead
+
     }};
 
     private Vector2 position;
     private Rectangle rectangle;
     private double currentTime = 0;
     public boolean isPaused = true;
+    public boolean isVisible = true;
 
     @Override
     public void render(SpriteBatch spriteBatch, ShapeRenderer shapeRenderer) {
         int borderThickness = 4;
 
-        if (!isPaused)
-            currentTime += Gdx.graphics.getDeltaTime() * getSpeed();
+        if (!isVisible) return;
+        if (!isPaused) currentTime += Gdx.graphics.getDeltaTime() * getSpeed();
 
         Rectangle size = sizes.get(currentTime);
         rectangle = new Rectangle(
@@ -85,9 +121,9 @@ public class SafeZone extends Component {
 
     private double getSpeed() {
         return switch (Game.instance.difficulty) {
-            case Difficulty.EASY -> 0.8;
-            case Difficulty.NORMAL -> 1;
-            case Difficulty.HARD -> 1.5;
+            case Difficulty.EASY -> 0.4;
+            case Difficulty.NORMAL ->1;
+            case Difficulty.EXTREME -> 1.5;
         };
     }
 }
